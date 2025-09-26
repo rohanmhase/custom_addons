88acd70db364b332cfb75383f422fd6d4f7cf441
rohanmhase <rohanmhase01122@gmail.com>
2025-09-26 14:55:52 +0530

    add clinic management folder with init and manifest file

---
+++ custom_addons/clinic_management/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Clinic Management",
+    "version": "1.0",
+    "category": "Healthcare",
+    "summary": "Manage Clinics Details",
+    "description": """
+        Module to manage Clinics
+    """,
+    "author": "Rohan Mhase",
+    "depends": ["base"],
+    "data": [],
+    "assets": {},
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
