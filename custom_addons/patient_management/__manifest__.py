def585620c638583e678016b691ce199968cf476
rohanmhase <rohanmhase01122@gmail.com>
2025-09-29 12:15:51 +0530

    Add patient management folder as application

---
+++ custom_addons/patient_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Patient Management",
+    "version": "1.0",
+    "category": "Healthcare",
+    "summary": "Manage patients in hospital",
+    "description": """
+        Module to manage patients:
+        - Create patients
+        - Store patient details
+    """,
+    "author": "Researchayu",
+    "depends": ["base", "clinic_management"],
+    'data': [],
+    'assets': {},
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
