078eabb69c02cbd2b2cd295dc9c9f3c44cd846f0
Parikshit001 <parikshithiwase@gmail.com>
2026-04-16 17:03:29 +0530

    feat: implement native OIDC identity provider for Moodle SSO

---
+++ custom_addons/sso_provider/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'OAuth2 Identity Provider',
+    'version': '17.0.1.0.0',
+    'summary': 'Turns Odoo into an Identity Provider for Moodle SSO',
+    'category': 'Tools',
+    'author': 'Your Name',
+    'depends': ['base', 'web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/oauth_client_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
