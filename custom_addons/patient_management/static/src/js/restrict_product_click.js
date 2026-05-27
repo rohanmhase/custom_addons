b646eb0710a998030441c8ee30d100e2828336a7
rohanmhase <rohanmhase01122@gmail.com>
2026-05-27 15:27:19 +0530

    feature: update enrollment form

--- custom_addons/patient_management/static/src/js/restrict_product_click.js
+++ custom_addons/patient_management/static/src/js/restrict_product_click.js
@@ -6,6 +6,37 @@ import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
 import { _t } from "@web/core/l10n/translation";
 
 patch(Order.prototype, {
+
+    set_partner(partner) {
+
+        const oldPartner = this.get_partner();
+
+        super.set_partner(partner);
+
+        // CUSTOMER CHANGED
+        if (
+            oldPartner &&
+            (
+                !partner ||
+                oldPartner.id !== partner.id
+            )
+            ) {
+
+            // CLEAR PRESCRIPTION
+            this.prescription_id = false;
+
+            // CLEAR ENROLLMENT
+            this.enrollment_id = false;
+
+            /// REMOVE ALL ORDERLINES
+            const lines = [...this.get_orderlines()];
+
+            lines.forEach(line => {
+                this.removeOrderline(line);
+            });
+        }
+    },
+
     async add_product(product, options) {
 
         // ✅ STEP 1: Allow prescription products
@@ -17,7 +48,7 @@ patch(Order.prototype, {
             return super.add_product(...arguments);
         }
 
-        if (product.type === 'service') {
+        if (options?.from_enrollment) {
             return super.add_product(...arguments);
         }
 
