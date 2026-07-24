18b71db6bf872fc1031e1578423de3ac9e119ce7
Sarthakmali <sarthakmaliuqpq@gmail.com>
2026-07-24 08:07:10 +0000

    feat/add- medicine price Vs Sales Module

--- custom_addons/account_automation/models/__init__.py
+++ custom_addons/account_automation/models/__init__.py
@@ -4,4 +4,4 @@ from . import bank_hub_config
 from . import psmr_mapping
 from . import bank_hub_audit
 from . import bank_hub_reason
-from . import clinic_performance,invoice_lock, invoice_lock_wizard
+from . import clinic_performance,invoice_lock, invoice_lock_wizard, medicine_transfer_sales_audit
