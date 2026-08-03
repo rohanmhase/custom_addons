7cc6e717ba9596a9e374576d5a5ba457be6a6963
Sarthakmali <sarthakmaliuqpq@gmail.com>
2026-08-03 11:13:21 +0000

    feat/change customer invoice to bill of supply

---
+++ custom_addons/bill_of_supply/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Bill of Supply',
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'POS invoices as Bill of Supply, Invoicing menu invoices as Tax Invoice',
+    'depends': ['account'],  # POS is optional, hasattr check handles it
+    'data': [
+        'views/report_invoice_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
